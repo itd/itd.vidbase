296c9adac51824f3ee14a814fc0eb07cf19599dc
zniper <zniper@localhost.localdomain>
2010-09-30 02:00:55 +0700

    Adding event for writing video to file system

--- itd/vidbase/config.py
+++ itd/vidbase/config.py
@@ -2,3 +2,4 @@ GLOBALS = globals()
 LAYER_NAME = 'itd_vidbase'
 PROJECT_NAME = 'itd.vidbase'
 PORTLET_NAME = u''
+ITDVIDEO_STORAGE_PATH = './'
--- itd/vidbase/content/itdvideo.py
+++ itd/vidbase/content/itdvideo.py
@@ -4,11 +4,14 @@ from plone.namedfile import field as namedfile
 
 from plone.directives import form, dexterity
 from zope.app.container.interfaces import IObjectAddedEvent
+from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 from Products.CMFCore.utils import getToolByName
-
+from zLOG import LOG, INFO, ERROR
 from datetime import datetime
+from os.path import join
 
 from itd.vidbase import _
+from itd.vidbase.config import ITDVIDEO_STORAGE_PATH
 
 class IITDVideo(form.Schema):
     """
@@ -46,6 +49,27 @@ def titleDefaultValue(data):
 def autoPublish(itdvideo, event):
     portal_workflow = getToolByName(itdvideo, 'portal_workflow')
     portal_workflow.doActionFor(itdvideo, 'publish')
+    write2FS(itdvideo, None)
+
+@grok.subscribe(IITDVideo, IObjectModifiedEvent)
+def write2FS(itdvideo, event):
+    """ This trigger method writes the video file to file system under path
+        specified in config.py 
+    """
+    video_obj = itdvideo.itdvideofile
+    file_path = join(ITDVIDEO_STORAGE_PATH, video_obj.filename)
+    try:
+        vf = open(file_path, 'w')
+        try:
+            vf.write(video_obj.data)
+        except IOError:
+            msg = 'Error occurs when writing file %s to disk' % \
+                  video_obj.filename 
+            LOG('itd.vidbase', ERROR, msg)
+        finally:
+            vf.close()
+    except IOError:
+        LOG('itd.vidbase', ERROR, 'Error occurs when creating file on disk')
 
 # the view class def
 #class View(grok.View):
