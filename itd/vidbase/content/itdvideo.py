2237fbbbff9dba39a1dcd247d9284afb064424be
Kurt Bendl <kurt@tool.net>
2010-09-30 01:14:41 -0600

    Set the folder/file os writes relative to the site root, using the path defined in config.py.

--- itd/vidbase/content/itdvideo.py
+++ itd/vidbase/content/itdvideo.py
@@ -1,3 +1,4 @@
+import os
 from five import grok
 from zope import schema
 from plone.namedfile import field as namedfile
@@ -9,6 +10,7 @@ from Products.CMFCore.utils import getToolByName
 from zLOG import LOG, INFO, ERROR
 from datetime import datetime
 from os.path import join
+from Acquisition import aq_inner
 
 from itd.vidbase import _
 from itd.vidbase.config import ITDVIDEO_STORAGE_PATH
@@ -44,7 +46,7 @@ def showdateDefaultValue(data):
 def titleDefaultValue(data):
     today = datetime.today()
     return today.strftime('%B, %d, %Y')
-    
+
 @grok.subscribe(IITDVideo, IObjectAddedEvent)
 def autoPublish(itdvideo, event):
     portal_workflow = getToolByName(itdvideo, 'portal_workflow')
@@ -54,22 +56,34 @@ def autoPublish(itdvideo, event):
 @grok.subscribe(IITDVideo, IObjectModifiedEvent)
 def write2FS(itdvideo, event):
     """ This trigger method writes the video file to file system under path
-        specified in config.py 
+        specified in config.py
     """
+    parent_path = '/'.join(itdvideo.aq_inner.aq_parent.getPhysicalPath())
     video_obj = itdvideo.itdvideofile
-    file_path = join(ITDVIDEO_STORAGE_PATH, video_obj.filename)
+    file_path = ITDVIDEO_STORAGE_PATH+parent_path+video_obj.filename
+    from pdb import set_trace; set_trace()
+    if not os.access(ITDVIDEO_STORAGE_PATH+parent_path, 1):
+        try:
+            os.makedirs(ITDVIDEO_STORAGE_PATH+parent_path)
+        except OSError:
+            msg = 'Error occured creating dir: %s for %s' %
+                (ITDVIDEO_STORAGE_PATH+parent_path, video_obj.filename )
+            LOG('itd.vidbase', ERROR, msg)
+
     try:
         vf = open(file_path, 'w')
         try:
             vf.write(video_obj.data)
         except IOError:
             msg = 'Error occurs when writing file %s to disk' % \
-                  video_obj.filename 
+                  video_obj.filename
             LOG('itd.vidbase', ERROR, msg)
         finally:
             vf.close()
     except IOError:
-        LOG('itd.vidbase', ERROR, 'Error occurs when creating file on disk')
+        LOG('itd.vidbase', ERROR, 'Error occurs when creating file on disk '+video_obj.filename)
+
+
 
 # the view class def
 #class View(grok.View):
