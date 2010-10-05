2fa271ef7947e9b1bb009bdb52d79bcd0f68c235
Kurt Bendl <kurt@tool.net>
2010-10-05 00:45:37 -0600

    Logic to handle None issue when there is no targetUrlSuffix.

--- itd/vidbase/browser/itdvfolderxml.py
+++ itd/vidbase/browser/itdvfolderxml.py
@@ -3,6 +3,7 @@ from zope.interface import Interface
 from zope.interface import implements
 from zope.component import getMultiAdapter
 from urllib import quote
+from Acquisition import aq_inner
 
 from Products.CMFCore.utils import getToolByName
 
@@ -13,24 +14,27 @@ class IXMLByShowDateView(Interface):
 class IXMLByFolderOrderView(Interface):
     """Marker interface identifying XMLByFolderOrderView View.
     """
-    
+
 class IXMLByFolderOrderReversedView(Interface):
     """Marker interface identifying XMLByFolderOrderReversedView View.
     """
 
 class ITDBaseXML(BrowserView):
-    """ Base class for XML generating 
+    """ Base class for XML generating
     """
-    
+
     def folderName(self):
         return self.context.title
-        
+
     def getTargetURL(self):
         return self.context.targeturl
-        
+
     def getTargetURLSuffix(self):
-        return self.context.targeturlsuffix
-        
+        suff = self.context.targeturlsuffix
+        if suff == None:
+            suff = ''
+        return suff.strip()
+
     def getUploadFileName(self, video_id):
         """ Returns the exact file name of itdvideo object
         """
@@ -46,28 +50,30 @@ class XMLByShowDateView(ITDBaseXML):
     """
     """
     implements(IXMLByShowDateView)
-    
+
     def subItems(self):
         """ Returns set of published itdvideo sorted by showdate
-        """       
+        """
+        context = aq_inner(self.context)
         itdFilter = {'portal_type':['itd.vidbase.itdvideo'],
                      'review_state':'published',
                      'sort_on':'showdate',
-                     'sort_order':'descending'}
+                     'sort_order':'descending',
+                     }
         items = self.context.getFolderContents(contentFilter=itdFilter)
         if self.context.limit > 0:
             items = items[:self.context.limit]
         return items
-        
+
 
 class XMLByFolderOrderView(ITDBaseXML):
     """
     """
     implements(IXMLByFolderOrderView)
-    
+
     def subItems(self):
         """ Returns set of published itdvideo by folder order
-        """       
+        """
         itdFilter = {'portal_type':['itd.vidbase.itdvideo'],
                      'review_state':'published',}
         items = self.context.getFolderContents(contentFilter=itdFilter)
@@ -80,10 +86,10 @@ class XMLByFolderOrderReversedView(ITDBaseXML):
     """
     """
     implements(IXMLByFolderOrderReversedView)
-    
+
     def subItems(self):
         """ Returns set of published itdvideo reverved to normal order
-        """       
+        """
         itdFilter = {'portal_type':['itd.vidbase.itdvideo'],
                      'review_state':'published',
                      'sort_order':'descending'}
