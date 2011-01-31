6aa259e16a71606f7f3d97320300dd4620c1e9f9
Kurt Bendl <kurt@tool.net>
2011-01-30 21:43:25 -0700

    Setting path to production value.

--- itd/vidbase/config.py
+++ itd/vidbase/config.py
@@ -2,4 +2,5 @@ GLOBALS = globals()
 LAYER_NAME = 'itd_vidbase'
 PROJECT_NAME = 'itd.vidbase'
 PORTLET_NAME = u''
-ITDVIDEO_STORAGE_PATH = '/web/msworks/vid/static'
+ITDVIDEO_STORAGE_PATH = '/home/msworks/www/kyxml'
+#ITDVIDEO_STORAGE_PATH = '/web/msworks/vid/static'
