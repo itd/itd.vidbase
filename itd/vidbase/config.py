9cd1444a4ad18d0e388822a3834c6cbc01094596
KurtB <kurt@tool.net>
2012-06-30 18:26:36 +0000

    Set ITDVIDEO_STORAGE_PATH in config for production.

--- itd/vidbase/config.py
+++ itd/vidbase/config.py
@@ -2,5 +2,6 @@ GLOBALS = globals()
 LAYER_NAME = 'itd_vidbase'
 PROJECT_NAME = 'itd.vidbase'
 PORTLET_NAME = u''
-ITDVIDEO_STORAGE_PATH = '/home/msworks/www/kyxml'
+ITDVIDEO_STORAGE_PATH = '/mnt/media/vid'
+#ITDVIDEO_STORAGE_PATH = '/home/msworks/www/kyxml'
 #ITDVIDEO_STORAGE_PATH = '/web/msworks/vid/static'
